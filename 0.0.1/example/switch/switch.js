270020a8cd4e7ca9b0e8931a5bbde3fd2f23e9ab
Farhan Apriliandi <apriliandi642@gmail.com>
2024-01-06 13:31:36 +0700

    add new example (switch)

---
+++ 0.0.1/example/switch/switch.js
@@ -0,0 +1,27 @@
+import { Interaction } from "../../core/interact.js";
+
+const CONTROLLER_NAME = "switch";
+
+const dataSchema = {
+	controller: "data-interaction-controller",
+	target: `data-${CONTROLLER_NAME}-target`,
+	event: `data-${CONTROLLER_NAME}-event`,
+};
+
+class Switch {
+	constructor() {
+		this.isOn = false;
+	}
+
+	toggle() {
+		if (this.isOn === false) {
+			this.isOn = true;
+			this.eventElement.setAttribute("aria-checked", "true");
+		} else {
+			this.isOn = false;
+			this.eventElement.setAttribute("aria-checked", "false");
+		}
+	}
+}
+
+new Interaction(CONTROLLER_NAME, dataSchema, Switch);
